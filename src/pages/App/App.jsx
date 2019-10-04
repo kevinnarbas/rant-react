5dcd9c94769ad7c3448a5a7de2ad96709e5b2130
kevinnarbas <kevinnarbas@git.generalassemb.ly>
2019-10-04 13:32:07 -0700

    adds initial deployment items

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
+      <img src="https://miro.medium.com/max/762/1*KVSXYX5Owz7-_5Rm_jHEKQ.jpeg" alt="REACT MEME"/>
     </div>
   );
 }
