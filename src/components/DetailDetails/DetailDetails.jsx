642160e3b76797acadd21aa3fffd0e0f9876fe0c
kevinnarbas <kevinnarbas@git.generalassemb.ly>
2019-10-08 08:55:01 -0700

    small refactor of detail page to break down components

---
+++ src/components/DetailDetails/DetailDetails.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+import {Accordion, Card, Button} from 'react-bootstrap'
+
+const DetailDetails = (props) => {
+  const {location, is_closed, display_phone, price} = props.location
+  const [line1, line2] = location.display_address
+  return (
+    <Accordion defaultActiveKey="0">
+      <Card>
+        <Accordion.Toggle as={Card.Header} eventKey="0">
+          Main Details
+        </Accordion.Toggle>
+        <Accordion.Collapse eventKey="0">
+          <Card.Body>
+            <h3>{is_closed ? 'Now Closed' : 'Now Open'}</h3>
+            <div>{line1}</div>
+            <div>{line2}</div>
+            <div>{display_phone}</div>
+            <div>{price}</div>
+            <Link to='/'>
+              <Button style={{backgroundColor: '#FF5252'}}>BACK</Button>
+            </Link>
+          </Card.Body>
+        </Accordion.Collapse>
+      </Card>
+      <Card>
+        <Accordion.Toggle as={Card.Header} eventKey="1">
+          Other Details
+        </Accordion.Toggle>
+        <Accordion.Collapse eventKey="1">
+          <Card.Body>
+            Hours of operation
+          </Card.Body>
+        </Accordion.Collapse>
+      </Card>
+      <Card>
+        <Accordion.Toggle as={Card.Header} eventKey="2">
+          Wanna RANT about this place?
+        </Accordion.Toggle>
+        <Accordion.Collapse eventKey="2">
+          <Card.Body>
+            Form here
+          </Card.Body>
+        </Accordion.Collapse>
+      </Card>
+    </Accordion>
+  )
+}
+
+
+export default DetailDetails
--- src/pages/BusinessDetailPage/BusinessDetailPage.jsx
+++ src/pages/BusinessDetailPage/BusinessDetailPage.jsx
@@ -1,18 +1,17 @@
 import React from 'react'
 import './BusinessDetailPage.css'
 import {Link} from 'react-router-dom'
-import Container from 'react-bootstrap/Container'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
+import {Container, Row, Col, Button, Accordion, Card} from 'react-bootstrap'
+import DetailDetails from '../../components/DetailDetails/DetailDetails'
 
 const BusinessDetailPage = (props) => {
-  const {name, is_closed, display_phone, image_url, price, location} = props.location.state.businesses
-  const [line1, line2] = location.display_address
+  const {name, image_url} = props.location.state.businesses
   return (
     <>
       <h1>{name}</h1>
+      <hr />
       <Container>
-      <Row>
+        <Row>
           <Col xs={4}><h5>Wait time:</h5> Overall Rating</Col>
           <Col xs={4}><h5>Wait Service:</h5> Overall Rating</Col>
           <Col xs={4}><h5>Food Taste:</h5> Overall Rating</Col>
@@ -22,19 +21,13 @@ const BusinessDetailPage = (props) => {
           <Col xs={4}><h5>Cleanliness:</h5> Overall Rating</Col>
           <Col xs={4}><h5>Bathrooms:</h5> Overall Rating</Col>
         </Row>
+        <hr />
         <Row>
           <Col xs={6}>
             <img className="Detail-img" src={image_url} alt="restauraunt-type"/>
           </Col>
           <Col sx={6}>
-            <h3>{is_closed ? 'Now Closed' : 'Now Open'}</h3>
-            <div>{line1}</div>
-            <div>{line2}</div>
-            <div>{display_phone}</div>
-            <div>{price}</div>
-            <Link to='/'>
-              <div>BACK</div>
-            </Link>
+            <DetailDetails location={props.location.state.businesses} />
           </Col>
         </Row>
         <Row>
